8f1cb1dc8fec7190ed4382aa46b7b99924ca7f1c
Joao Stein <joaosteing@gmail.com>
2022-10-05 14:13:40 +0200

    feat: implement initial test suite

--- test/BuyMeACoffee.js
+++ test/BuyMeACoffee.js
@@ -1,5 +1,15 @@
 const { expect } = require('chai');
 
 describe('BuyMeACoffee', function () {
+  let owner, bob, alice, john, buyMeACoffee;
 
+  beforeEach(async () => {
+    [owner, bob, alice, john] = await ethers.getSigners();
+    const BuyMeACoffee = await ethers.getContractFactory("BuyMeACoffee");
+    buyMeACoffee = await BuyMeACoffee.connect(owner).deploy();
+  });
+
+  it('should exist', () => {
+    expect(buyMeACoffee).to.exist;
+  });
 });
