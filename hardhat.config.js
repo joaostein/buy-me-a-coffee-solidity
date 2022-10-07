07660126331a87fe796dd4537ffe2c949bce81bc
Joao Stein <joaosteing@gmail.com>
2022-10-07 11:27:41 +0200

    install `dotenv` and configure testnet deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: {
@@ -9,6 +13,12 @@ module.exports = {
           "*": ["storageLayout"]
         }
       }
+    }
+  },
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
     }
   }
 };
